4d22f6a72c25ef7b58d086a94ef0d61bb20793e0
HeflerDev <heflerdev@gmail.com>
2021-01-07 01:21:35 -0300

    structure/folders

---
+++ src/modules/render.js
@@ -0,0 +1,23 @@
+const render = (() => {
+  const listeners = {
+    about: document.getElementById('btn-about'),
+    featured: document.getElementById('btn-featured'),
+    portfolio: document.getElementById('btn-portfolio'),
+    contact: document.getElementById('btn-contact')
+  };
+
+  const _createElement = (elId, elParent, elType = null, elClass = null) => {    
+    // Should create an element
+  };
+
+  const aboutTab = () => {
+    // Should render the aboutTab
+  };
+
+  return {
+    listeners
+  }
+
+})();
+
+export default render;
